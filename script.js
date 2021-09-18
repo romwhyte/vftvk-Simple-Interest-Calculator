6d65991917c00cc869f4f1b67c60269879f9bfce
romwhyte <rom_whyte@hotmail.com>
2021-09-18 09:35:23 -0500

    Update for js

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var rate = 0.5;
+
+function compute() {
+    resultdoc = document.getElementById("displayResult");
+    resultdoc.style.visibility = "visible";
+    principal = document.getElementById("principal").value;
+    year = document.getElementById('yearId').value;
+    result = document.getElementById('result');
+
+    /* calcul du nombre d'année*/
+    var today = new Date();
+    var annee = today.getFullYear();
+    var result1 = ((annee = +year));
+    var today2 = new Date();
+    var annee2 = today2.getFullYear();
+    var result2 = result1 + annee2
+
+    /* calul et affichage de l'intérêt*/
+    result.innerHTML = "If you deposit" + "&nbsp;" + (principal) + "," + "<br/>" + "at an interest rate of" + "&nbsp;" + (rate) + "." + "<br/>" +
+        "You will receive an amount of" + "&nbsp;" + (principal * rate * year / 100) + "," + "<br/>" +
+        "in the year" + "&nbsp;" + (result2);
 }
-        
+
+function rateChange(value) {
+   rate =  value;
+   rateId = document.getElementById("rateId");
+   rateId.innerHTML = ""+value;
+   console.log(""+value);
+}
